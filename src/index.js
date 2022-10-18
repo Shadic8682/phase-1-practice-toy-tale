2d559afd15526880acc21c3aae566ebb9f7145ee
Lianzi <lianziliu.sugar@gmail.com>
2022-10-18 13:32:52 -0400

    add heart button

--- src/index.js
+++ src/index.js
@@ -15,12 +15,12 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 
-  const toyCollectionDiv= document.getElementById("toy-collection")
+
 
 // GET the toys from JSON-server/toys
 fetch ("http://localhost:3000/toys")
 .then (r => r.json())
-.then (  data => {
+.then ( data => {
 
   const toyObj = data
 //Render / display all of the toys in the created card
@@ -29,18 +29,28 @@ fetch ("http://localhost:3000/toys")
     //console.log (eachToy)
     //createElements
     const divForCard= document.createElement ("div")
-    divForCard.className = "card";
+    divForCard.className = "card"
+
     const h2ForCard = document.createElement("h2")
     h2ForCard.textContent = eachToy.name
+
     const imgForCard = document.createElement("img")
     imgForCard.className = "toy-avatar"
     imgForCard.src = eachToy.image
+
     const pForCard = document.createElement("p")
     pForCard.textContent = `${eachToy.likes} likes`
+
     const btnForCard = document.createElement("button")
     btnForCard.className = "like-btn"
     btnForCard.id = eachToy.id
     btnForCard.innerText = "Like ❤️"
+    // increment likes on the card
+    btnForCard.addEventListener ("click", () => {
+        const newLikes = ++ eachToy.likes
+        pForCard.textContent = `${newLikes} likes`
+
+    })
 
     divForCard.append (h2ForCard, imgForCard, pForCard, btnForCard)
     toyCollectionDiv.append (divForCard)
@@ -49,11 +59,11 @@ fetch ("http://localhost:3000/toys")
 
 })
 
+const toyCollectionDiv= document.getElementById("toy-collection")
 
 
 //make the form take value and push info to the card
 
-// increment likes on the card
 
 
 //
