362538042b6d8d802ad18f6a3bfd5dcaa3c79ef0
JaQuan Banks <shadic151@gmail.com>
2022-10-18 11:03:29 -0400

    Added submit listener.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const addCard = document.querySelector(".submit");
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+addCard.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("I've been submitted.")
+})});
